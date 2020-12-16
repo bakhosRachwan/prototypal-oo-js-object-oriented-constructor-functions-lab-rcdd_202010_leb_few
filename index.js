16b0bf8177f652fcf5b0a91824eb787cd2f1edb1
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-16 08:14:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const Scooter = (year, color, model) => {
+Scooter {
+constructor = (year, color, model){
   this.year = year;
   this.color = color;
   this.model = model;
-};
-
+  }
+}
 const Driver = (name, age, experience) => {
   this.name = name;
   this.age = age;
